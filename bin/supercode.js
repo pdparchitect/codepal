3c2f07f123fb53bcb84a9f4476759a2e7c3391d8
pdparchitect <pdp@gnucitizen.org>
2022-12-06 17:22:45 +0000

    First.

---
+++ bin/supercode.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+
+const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
+const { v4: uuidv4 } = require('uuid')
+
+const { getOpenAI, Conversation } = require('../lib/index.js')
+
+async function main() {
+  let conversationPath
+
+  await fs.promises.mkdir('.superchat', { recursive: true })
+
+  const conversation = new Conversation({
+    openai: getOpenAI(process.env.OPENAI_API_KEY)
+  })
+
+  if (process.argv[2]) {
+    let conversationContents
+
+    for (const file of [process.argv[2], path.join('.superchat', process.argv[2]), path.join('.superchat', process.argv[2] + '.md')]) {
+      try {
+        conversationPath = file
+        conversationContents = await fs.promises.readFile(file)
+
+        break
+      } catch (e) {}
+    }
+
+    if (!conversationContents) {
+      console.error(`[x] session ${process.argv[2]} not found`)
+
+      process.exit(1)
+    }
+
+    await conversation.loadConversation(conversationContents.toString())
+  } else {
+    conversationPath = path.join('.superchat', `${uuidv4()}.md`)
+  }
+
+  while (true) {
+    const question = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'message',
+        message: 'Q',
+      }
+    ]);
+
+    if (question.message.toLowerCase() === 'exit') {
+      break;
+    }
+
+    process.stdout.write('\n')
+
+    for await (const token of conversation.interact(question.message)) {
+        process.stdout.write(token)
+    }
+
+    process.stdout.write('\n')
+
+    await fs.promises.writeFile(conversationPath, conversation.dumpConversation())
+  }
+}
+
+main()
