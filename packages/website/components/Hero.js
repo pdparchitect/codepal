43e75ee1d9f8d78c4a60195483262c444761f615
pdparchitect <pdp@gnucitizen.org>
2022-12-08 16:47:28 +0000

    Improved the heading.

--- packages/website/components/Hero.js
+++ packages/website/components/Hero.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Hero = ({ title, subTitle }) => (
-  <div className="bg-gray-200 py-50">
+  <div className="bg-black py-10 text-white">
     <div className="max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold">{title}</h1>
+      <h1 className="text-3xl font-bold">{title}</h1>
       <p className="text-gray-600">{subTitle}</p>
     </div>
   </div>
