aaf79285bfbe59c95ee4b27a5916b29eacd02e9d
pdparchitect <pdp@gnucitizen.org>
2022-12-08 09:47:00 +0000

    Improved file file lookup and fixed console out.

--- bin/codepal.js
+++ bin/codepal.js
@@ -11,8 +11,16 @@ const { hideBin } = require('yargs/helpers')
 
 const { getOpenAI, Conversation } = require('../lib/index.js')
 
+function getRoot() {
+  return '.codepal'
+}
+
+function getRootPath(...args) {
+  return path.join(getRoot(), ...args)
+}
+
 async function main() {
-  const { interactive, session, template } = yargs(hideBin(process.argv))
+  const { interactive, session, template, file } = yargs(hideBin(process.argv))
     .option('interactive', {
       alias: 'i',
       type: 'boolean',
@@ -23,17 +31,22 @@ async function main() {
       type: 'string',
       description: 'Session id/file to resume'
     })
-    .options('tempalte', {
+    .options('template', {
       alias: 't',
       type: 'string',
       description: 'Template name/file to use'
+    })
+    .options('file', {
+      alias: 'f',
+      type: 'string',
+      description: 'Start from a file'
     })
     .parse()
 
   let backstory
 
   if (template) {
-    for (const file of [template, path.join('.codepal', 'templates', template), path.join('.codepal', 'templates', template + '.md')]) {
+    for (const file of [template, getRootPath('templates', template), getRootPath('templates', template + '.md')]) {
       try {
         backstory = (await fs.promises.readFile(file)).toString()
 
@@ -42,10 +55,24 @@ async function main() {
     }
 
     if (!backstory) {
-      console.error(`[x] template ${template} not found`)
+      console.error(`x template ${template} not found`)
 
       process.exit(1)
     }
+  }
+
+  if (file) {
+    let fileContents
+    
+    try {
+      (await fs.promises.readFile(file)).toString()
+    } catch (e) {
+      console.error(`x file ${file} not found`)
+
+      process.exit(1)
+    }
+
+    backstory = `${backstory ? backstory + '\n\n' : ''}${fileContents}`
   }
 
   if (!process.env.OPENAI_API_KEY) {
@@ -59,8 +86,8 @@ async function main() {
   })
 
   await Promise.all([
-    fs.promises.mkdir(path.join('.codepal', 'sessions'), { recursive: true }),
-    fs.promises.mkdir(path.join('.codepal', 'templates'), { recursive: true })
+    fs.promises.mkdir(getRootPath('sessions'), { recursive: true }),
+    fs.promises.mkdir(getRootPath('templates'), { recursive: true })
   ])
 
   let sessionPath
@@ -68,7 +95,7 @@ async function main() {
   if (session) {
     let sessionContents
 
-    for (const file of [session, path.join('.codepal', 'sessions', session), path.join('.codepal', 'sessions', session + '.md')]) {
+    for (const file of [session, getRootPath('sessions', session), getRootPath('sessions', session + '.md')]) {
       try {
         sessionPath = file
         sessionContents = (await fs.promises.readFile(file)).toString()
@@ -78,7 +105,7 @@ async function main() {
     }
 
     if (!sessionContents) {
-      console.error(`[x] session ${session} not found`)
+      console.error(`x session ${session} not found`)
 
       process.exit(1)
     }
